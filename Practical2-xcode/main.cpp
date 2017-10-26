0d191a395a06da93d31508406455db739680766b
LesLunettes <adrienpourtier@orange.fr>
2017-10-26 23:36:37 +0100

    init

--- Practical2-xcode/main.cpp
+++ Practical2-xcode/main.cpp
@@ -1,15 +1,68 @@
-//
-//  main.cpp
-//  Practical2-xcode
-//
-//  Created by Adrien Pourtier on 26/10/2017.
-//  Copyright © 2017 Adrien Pourtier. All rights reserved.
-//
+/*
+ * main.cpp
+ *
+ *  Created on: 15 Oct 2014
+ *      Author: np183
+ */
 
 #include <iostream>
+#include <string>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+#include "VectorTester.h"
+
+//testcvbfghsdfdfg
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+int main(int argc, char* argv[]) {
+
+	if (false) {
+		for (unsigned int i=0 ; i<1 ; ++i) {
+			VectorTester t;
+			t.testAddGetElem();
+		}
+		return 0;
+
+	}
+
+	while (argc > 1) {
+
+		switch (argv[--argc][0]) {
+		case 'a':
+		{
+			VectorTester t;
+			t.testCtor();
+		}
+		break;
+		case 'b':
+		{
+			VectorTester t;
+			t.testAddGetElem();
+		}
+		break;
+		case 'c':
+		{
+			VectorTester t;
+			t.testSize();
+		}
+		break;
+		case 'd':
+		{
+			VectorTester t;
+			t.testSetElem();
+		}
+		break;
+		case 'e':
+		default:
+		{
+			VectorTester t;
+			t.testClear();
+		}
+		break;
+        }
+	}
+	return 0;
 }
+
